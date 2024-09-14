1c6b9f476e9b7ce182a19d0a510828930b1b080d
Igor Grabowski <dogodontexist@gmail.com>
2024-09-14 14:25:01 +0200

    add __filename & __dirname
    added them to maintain views engine

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,12 +1,28 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
+import url from "url";
 
 import { userRouter } from "./routes/userRoutes.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8081;
 const app = express();
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("hello", {
+    pageTitle: "Sign Up",
+  });
+});
 
 app.use("/user", userRouter);
 
