fd3d3d71608aca835e5fa5a91db2eac990bc1b03
Igor Grabowski <dogodontexist@gmail.com>
2024-09-14 13:10:58 +0200

    delete get route

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -6,12 +6,4 @@ dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({
-    data: {
-      message: "Hello World!",
-    },
-  });
-});
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
