180f6da11a9c38f9a42fcbf3351e1d6c54d1370e
Igor Grabowski <dogodontexist@gmail.com>
2024-09-15 12:42:28 +0200

    repair authRoutes

--- back-end/src/routes/authRoutes.ts
+++ back-end/src/routes/authRoutes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { createUser, signInUser } from "../controllers/userController.js";
+import {
+  createUser,
+  signInUser,
+  changePassword,
+} from "../controllers/authController.js";
 const router = Router();
 
 router.route("/register").post(createUser);
 router.route("/login").post(signInUser);
+router.route("/password").put(changePassword);
 
 export { router as authRouter };
