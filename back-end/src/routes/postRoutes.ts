4eac746115d5a9b6b75c3af157fc03d4e7125743
Igor Grabowski <dogodontexist@gmail.com>
2024-09-16 20:02:37 +0200

    impleent more routes

--- back-end/src/routes/postRoutes.ts
+++ back-end/src/routes/postRoutes.ts
@@ -7,16 +7,24 @@ import {
   updatePost,
   deletePost,
 } from "../controllers/postControllers.js";
+import {
+  createPostLike,
+  deletePostLike,
+  getPostLikeById,
+} from "../controllers/postLikesController.js";
 const router = Router();
 
 router.route("/").get(getPosts);
 router
   .route("/post")
-  .get(getPostById)
   .post(createPost)
+  .get(getPostById)
   .put(updatePost)
   .delete(deletePost);
-router.route("/posts-by-id").get(getPostsById);
+router.route("/get-by-id").get(getPostsById);
+
+router.route("/like").post(createPostLike).delete(deletePostLike);
+router.route("/like/get-by-id").get(getPostLikeById);
 
 export { router as postRouter };
-// /api/posts/post
+// /api/posts/
