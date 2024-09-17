57a13d56a7fe4b6b24b9f5086575564b8690ead1
Igor Grabowski <dogodontexist@gmail.com>
2024-09-17 21:02:54 +0200

    implement comment likes controllers & routes

--- back-end/src/controllers/commentLikesController.ts
+++ back-end/src/controllers/commentLikesController.ts
@@ -1,1 +1,184 @@
-// CREATE | READ | DELETE
+/// CREATE | READ | DELETE ///
+import { Request, Response } from "express";
+import prisma from "../prisma/prismaClient.js";
+
+const createCommentLike = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { commentId, userId } = req.body;
+  try {
+    const existingComment = await prisma.comments.findUnique({
+      where: {
+        id: Number(commentId),
+      },
+    });
+    if (!existingComment) {
+      res.status(404).json({
+        data: {
+          status: 404,
+          message: "Comment not found",
+        },
+      });
+      return;
+    }
+    const existingUser = await prisma.users.findUnique({
+      where: {
+        id: Number(userId),
+      },
+    });
+    if (!existingUser) {
+      res.status(404).json({
+        data: {
+          status: 404,
+          message: "User not found",
+        },
+      });
+      return;
+    }
+
+    const existingLike = await prisma.comment_likes.findFirst({
+      where: {
+        AND: [{ comment_id: Number(commentId) }, { user_id: Number(userId) }],
+      },
+    });
+    if (existingLike) {
+      res.status(409).json({
+        data: {
+          status: 409,
+          message: "Like already exists",
+        },
+      });
+      return;
+    }
+
+    const createdCommentLike = await prisma.comment_likes.create({
+      data: {
+        comment_id: Number(commentId),
+        user_id: Number(userId),
+      },
+    });
+
+    res.status(200).json({
+      data: {
+        status: 200,
+        createdCommentLike,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      data: {
+        status: 500,
+        message: "Error fetching data",
+      },
+    });
+  }
+};
+
+const getCommentLikesByCommentId = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { commentId } = req.body;
+  try {
+    const existingComment = await prisma.comments.findUnique({
+      where: {
+        id: Number(commentId),
+      },
+    });
+    if (!existingComment) {
+      res.status(404).json({
+        data: {
+          status: 404,
+          message: "Comment not found",
+        },
+      });
+      return;
+    }
+
+    const commentLikes = await prisma.comment_likes.findMany({
+      where: {
+        comment_id: Number(commentId),
+      },
+    });
+
+    res.status(200).json({
+      data: {
+        status: 200,
+        commentLikes,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      data: {
+        status: 500,
+        message: "Error fetching data",
+      },
+    });
+  }
+};
+
+const getCommentLikesByUserId = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId } = req.body;
+  try {
+    const existingUser = await prisma.users.findUnique({
+      where: {
+        id: Number(userId),
+      },
+    });
+    if (!existingUser) {
+      res.status(404).json({
+        data: {
+          status: 404,
+          message: "User not found",
+        },
+      });
+      return;
+    }
+
+    const commentLikes = await prisma.comment_likes.findMany({
+      where: {
+        user_id: Number(userId),
+      },
+    });
+    res.status(200).json({
+      data: {
+        status: 200,
+        commentLikes,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      data: {
+        status: 500,
+        message: "Error fetching data",
+      },
+    });
+  }
+};
+
+const deleteCommentLike = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const {} = req.body;
+  try {
+  } catch (err) {
+    res.status(500).json({
+      data: {
+        status: 500,
+        message: "Error fetching data",
+      },
+    });
+  }
+};
+
+export {
+  createCommentLike,
+  getCommentLikesByUserId,
+  getCommentLikesByCommentId,
+  deleteCommentLike,
+};
--- back-end/src/routes/commentRoutes.ts
+++ back-end/src/routes/commentRoutes.ts
@@ -5,10 +5,19 @@ import {
   getCommentsByPostId,
   getCommentsByUserId,
 } from "../controllers/commentController.js";
+import {
+  createCommentLike,
+  deleteCommentLike,
+  getCommentLikesByCommentId,
+  getCommentLikesByUserId,
+} from "../controllers/commentLikesController.js";
 const router = Router();
 
 router.route("/").post(createComment).delete(deleteComment);
 router.route("/get-by-user-id").get(getCommentsByUserId);
 router.route("/get-by-post-id").get(getCommentsByPostId);
 
+router.route("/like").post(createCommentLike).delete(deleteCommentLike);
+router.route("/like/get-by-user-id").get(getCommentLikesByUserId);
+router.route("/like/get-by-comment-id").get(getCommentLikesByCommentId);
 export { router as commentRouter };
