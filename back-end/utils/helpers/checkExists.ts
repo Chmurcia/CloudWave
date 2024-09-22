a0b535201ce627b423e0828840b1209d1be69595
Igor Grabowski <dogodontexist@gmail.com>
2024-09-22 15:20:52 +0200

    improve

--- back-end/utils/helpers/checkExists.ts
+++ back-end/utils/helpers/checkExists.ts
@@ -1,6 +1,9 @@
 import { Response } from "express";
 import prisma from "../../src/prisma/prismaClient.js";
 
+// TO CAPITALIZE STRINGS
+const capitalize = (str: String) => str.charAt(0).toUpperCase() + str.slice(1);
+
 const checkUserExists = async (
   res: Response,
   id: number,
@@ -70,9 +73,86 @@ const checkThingExists409 = async <T>(
   return null;
 };
 
+const checkThingCUS409 = async <T>(
+  res: Response,
+  thing: T | null,
+  thingName: string,
+  state: string
+) => {
+  if (thing) {
+    switch (state) {
+      case "owner":
+        res.status(409).json({
+          data: {
+            status: 409,
+            messages: {
+              message: `${capitalize(thingName)} cannot leave!`,
+              addMessage: "Delete chat or give owner to someone else",
+            },
+          },
+        });
+        return thing;
+      case "banned":
+        if (thingName === "owner" || thingName === "moderator") {
+          res.status(409).json({
+            data: {
+              status: 409,
+              message: `${capitalize(thingName)} cannot be banned!`,
+            },
+          });
+          return thing;
+        }
+        res.status(409).json({
+          data: {
+            status: 409,
+            message: `${thingName} is banned!`,
+          },
+        });
+        return thing;
+      case "unbanned":
+        res.status(409).json({
+          data: {
+            status: 409,
+            message: `${thingName} is not banned!`,
+          },
+        });
+        return thing;
+      case "muted":
+        if (thingName === "owner" || thingName === "moderator") {
+          res.status(409).json({
+            data: {
+              status: 409,
+              message: `${capitalize(thingName)} cannot be muted!`,
+            },
+          });
+          return thing;
+        }
+        res.status(409).json({
+          data: {
+            status: 409,
+            message: `${thingName} is muted!`,
+          },
+        });
+        return thing;
+      case "unmuted":
+        res.status(409).json({
+          data: {
+            status: 409,
+            message: `${thingName} is not muted!`,
+          },
+        });
+        return thing;
+    }
+  }
+  return null;
+};
+
 export {
   checkUserExists,
   checkThingExists400,
   checkThingExists404,
   checkThingExists409,
+  checkThingCUS409,
 };
+//message: "Owner cannot leave!",
+// addMessage: "Delete chat or give owner to someone else",
